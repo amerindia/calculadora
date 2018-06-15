afadc8ec35aeb6af53cab33ee52b28e0b2de8aa2
Alisson Patricio <eu@alisson.net>
2018-06-14 22:53:55 -0300

    melhoras no layout e tabela de informacoes para freq 1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,84 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import TextField from '@material-ui/core/TextField';
 import Freq1 from './frequencies/One.js';
 import Freq2 from './frequencies/Two.js';
+import './App.css';
+
+const styles = theme => ({
+  radius: {
+    textAlign: 'center'
+  },
+  freqTitle: {
+    textAlign: 'center',
+    paddingTop: '5px'
+  }
+});
 
 class App extends Component {
+  state = {
+    radius: ''
+  }
+
+  handleChange = name => e => {
+    this.setState({
+      [name]: parseFloat(e.target.value)
+    });
+  }
+
   render() {
+    const { classes } = this.props;
+
     return (
       <React.Fragment>
         <CssBaseline />
 
-        <List>
-          <ListItem button component={Link} to={'/frequencia/1'}>
-            <ListItemText primary="1" />
-          </ListItem>
-          <ListItem button component={Link} to={'/frequencia/2'}>
-            <ListItemText primary="2" />
-          </ListItem>
-        </List>
+        <Grid container spacing={24}>
+          <Grid item xs={12} className={classes.radius}>
+            <TextField
+              label="Raio da Cúpula"
+              type="number"
+              value={this.state.radius}
+              onChange={this.handleChange('radius')}
+              inputProps={{
+                min: 0
+              }}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <Typography variant="headline" className={classes.freqTitle}>
+              Frequencia:
+            </Typography>
+          </Grid>
+          <Grid item xs={10}>
+            <AppBar position='static'>
+              <Tabs value={0}>
+                <Tab label="1" component={Link} to={'/frequencia/1'} />
+                <Tab label="2" component={Link} to={'/frequencia/2'} />
+                <Tab label="3" component={Link} to={'/frequencia/3'} />
+                <Tab label="4" component={Link} to={'/frequencia/5'} />
+                <Tab label="5" component={Link} to={'/frequencia/5'} />
+                <Tab label="6" component={Link} to={'/frequencia/6'} />
+              </Tabs>
+            </AppBar>
+          </Grid>
+        </Grid>
 
-        <Route path='/frequencia/1' component={Freq1} />
-        <Route path='/frequencia/2' component={Freq2} />
+
+        <div className="frequencyResults">
+          <Route path='/frequencia/1' component={(props) => <Freq1 {...props} radius={this.state.radius} />} />
+          <Route path='/frequencia/2' component={(props) => <Freq2 {...props} radius={this.state.radius} />} />
+        </div>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/frequencies/One.js
+++ src/frequencies/One.js
@@ -1,36 +1,68 @@
 import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import numeral from 'numeral';
+import oneMap from './One.Map.png';
+
+const styles = theme => ({
+  waysConn: {
+    textAlign: 'right'
+  }
+});
 
 class Freq1 extends Component {
-  state = {
-    radius: ''
-  }
-
-  handleChange = name => e => {
-    this.setState({
-      [name]: e.target.value
-    });
-  }
-
   render() {
-    let strutA;
+    const { classes } = this.props;
 
-    if (this.state.radius) {
-      strutA = this.state.radius * 1.05146;
+    let strutA;
+    if (this.props.radius) {
+      strutA = this.props.radius * 1.05146;
     }
 
-    return <div>
-      <TextField
-        label="Dome Radius"
-        type="number"
-        value={this.state.radius}
-        onChange={this.handleChange('radius')}
-      />
-
-      Strut A: {numeral(strutA).format('0.0[00]')}       
-    </div>;
+    return <Grid container spacing={24}>
+      <Grid item xs={6}>
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Barra</TableCell>
+                <TableCell>Comprimento</TableCell>
+                <TableCell>Cúpula</TableCell>
+                <TableCell>Esfera</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>A</TableCell>
+                <TableCell>{numeral(strutA).format('0.0[00]')}</TableCell>
+                <TableCell>25</TableCell>
+                <TableCell>30</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell colSpan={2} className={classes.waysConn}>Conectores de 5 vias:</TableCell>
+                <TableCell>6</TableCell>
+                <TableCell>12</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell colSpan={2} className={classes.waysConn}>Conectores de 4 vias:</TableCell>
+                <TableCell>5</TableCell>
+                <TableCell>0</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <img src={oneMap} alt="Mapa aereo da frequencia 1" />
+      </Grid>
+    </Grid>;
   }
 }
 
-export default Freq1;
+export default withStyles(styles)(Freq1);
